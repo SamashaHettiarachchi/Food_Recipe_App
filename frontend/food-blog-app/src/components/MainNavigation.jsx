9383e5607e1f526dbaeee0bc471b1e0a7e2a4e46
SamashaHettiarachchi <sashinisamasha@gmail.com>
2025-08-09 22:10:48 +0530

    Create_MainNavigation

--- frontend/food-blog-app/src/components/MainNavigation.jsx
+++ frontend/food-blog-app/src/components/MainNavigation.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./footer";
 import { Outlet } from "react-router-dom";
+import "./MainNavigation.css";
 
 const MainNavigation = () => {
   console.log("MainNavigation component is rendering!"); // Debug log
   return (
-    <>
+    <div className="main-navigation">
       <Navbar />
-      <main>
+      <main className="main-content">
         <Outlet />
       </main>
-      <Footer />
-    </>
+      <div className="footer-container">
+        <Footer />
+      </div>
+    </div>
   );
 };
 
