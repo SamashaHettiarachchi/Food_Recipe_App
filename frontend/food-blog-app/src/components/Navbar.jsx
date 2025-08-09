1698ed0427085fe4a7d91abf78fa00972f14cd6c
SamashaHettiarachchi <sashinisamasha@gmail.com>
2025-08-09 10:04:10 +0530

    create_nav_bar

--- frontend/food-blog-app/src/components/Navbar.jsx
+++ frontend/food-blog-app/src/components/Navbar.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
+  console.log("Navbar component is rendering!"); // Debug log
   return (
     <>
-    <header>
-        <h2>Food Blog</h2>
-        <ul>
-            <li>Home</li>
-            <li>my Recipes</li>
-            <li>Favourite</li>
-            <li>Login</li>
+      <header
+        style={{
+          backgroundColor: "#ffffff",
+          padding: "10px",
+          borderBottom: "1px solid #ddd",
+        }}
+      >
+        <h2 style={{ color: "black", margin: 0 }}>Food Blog</h2>
+        <ul
+          style={{
+            display: "flex",
+            listStyle: "none",
+            gap: "20px",
+            margin: 0,
+            padding: 0,
+          }}
+        >
+          <li style={{ color: "black", cursor: "pointer" }}>Home</li>
+          <li style={{ color: "black", cursor: "pointer" }}>my Recipes</li>
+          <li style={{ color: "black", cursor: "pointer" }}>Favourite</li>
+          <li style={{ color: "black", cursor: "pointer" }}>Login</li>
         </ul>
-    </header>
+      </header>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
