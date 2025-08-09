33579aa49feee9b2d660f3c5acb04409987e991f
SamashaHettiarachchi <sashinisamasha@gmail.com>
2025-08-09 22:08:33 +0530

    create_Home_page

--- frontend/food-blog-app/src/pages/Home.jsx
+++ frontend/food-blog-app/src/pages/Home.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import foodimg from "../assets/Cake_image.jpg";
 import RecipeItems from "../components/RecipeItems";
+import "./Home.css";
 
 function Home() {
   return (
     <>
-      <section className="home">
-        <div className="left">
-          <h1>Food Recipe App</h1>
-          <h5>Your one-stop solution for all food recipes</h5>
-          <button>Share your recipe</button>
+      <section className="home-section">
+        <div className="home-left">
+          <h1 className="home-title">Food Recipe App</h1>
+          <h5 className="home-subtitle">
+            Your one-stop solution for all food recipes
+          </h5>
+          <button className="home-button">Share your recipe</button>
         </div>
-        <div className="right">
-          <img src={foodimg} alt="Food" width="320px" height="300px" />
+        <div className="home-right">
+          <img
+            src={foodimg}
+            alt="Food"
+            className="home-image"
+            width="320"
+            height="300"
+          />
         </div>
       </section>
-      <div className="recipe">
+      <div className="recipes-section">
+        <h2 className="recipes-title">Our Latest Recipes</h2>
         <RecipeItems />
       </div>
     </>
