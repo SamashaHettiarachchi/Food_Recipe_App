2e7be78e90dec7a10eaadb5ddc006673e03db33b
SamashaHettiarachchi <sashinisamasha@gmail.com>
2025-08-09 22:09:59 +0530

    change_navBar

--- frontend/food-blog-app/src/components/Navbar.jsx
+++ frontend/food-blog-app/src/components/Navbar.jsx
@@ -1,30 +1,17 @@
 import React from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
   console.log("Navbar component is rendering!"); // Debug log
   return (
     <>
-      <header
-        style={{
-          backgroundColor: "#ffffff",
-          padding: "10px",
-          borderBottom: "1px solid #ddd",
-        }}
-      >
-        <h2 style={{ color: "black", margin: 0 }}>Food Blog</h2>
-        <ul
-          style={{
-            display: "flex",
-            listStyle: "none",
-            gap: "20px",
-            margin: 0,
-            padding: 0,
-          }}
-        >
-          <li style={{ color: "black", cursor: "pointer" }}>Home</li>
-          <li style={{ color: "black", cursor: "pointer" }}>my Recipes</li>
-          <li style={{ color: "black", cursor: "pointer" }}>Favourite</li>
-          <li style={{ color: "black", cursor: "pointer" }}>Login</li>
+      <header className="navbar-header">
+        <h2 className="navbar-title">Food Blog</h2>
+        <ul className="navbar-menu">
+          <li className="navbar-item">Home</li>
+          <li className="navbar-item">my Recipes</li>
+          <li className="navbar-item">Favourite</li>
+          <li className="navbar-item">Login</li>
         </ul>
       </header>
     </>
