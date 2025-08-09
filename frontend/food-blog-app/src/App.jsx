bd6f4065320e8b40c368b7e22a57c8599ef2b29d
SamashaHettiarachchi <sashinisamasha@gmail.com>
2025-08-09 10:04:34 +0530

    change_app.js

--- frontend/food-blog-app/src/App.jsx
+++ frontend/food-blog-app/src/App.jsx
@@ -1,8 +1,26 @@
+import React from "react"
+import'./App.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Home from './pages/Home'
+import MainNavigation from './components/MainNavigation'
+import axios from 'axios'
+
+const getAllRecipes = async () => {
+  // Logic to fetch all recipes
+  let getAllRecipes = [];
+  await axios.get('http://localhost:5000/api/recipes').then(res=>{
+    getAllRecipes = res.data;
+  });
+  return getAllRecipes;
+}
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home/> },
+  {
+    element: <MainNavigation />,
+    children: [
+      { path: "/", element: <Home /> },
+    ]
+  }
 ])
 
 export default function App() {
